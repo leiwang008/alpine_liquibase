095e54de3ea06f9a6c7314fbaf1e48868cd85448
Lei Wang <lei.wang@sas.com>
2021-06-21 15:42:49 +0800

    modify batch script to start/stop postgres/liquibase

--- start.bat
+++ start.bat
@@ -10,6 +10,6 @@ docker run --network=database_network --name alpine_postgres --publish 5432:5432
 echo "Create a liquibase 'alpine_liquibase' in network 'database_network' to update database in 'alpine_postgres' container
 docker run --network=database_network --name alpine_liquibase --rm  leiwang008/alpine_liquibase update
 @REM docker run --network=database_network --name alpine_liquibase --rm -e LIQUIBASE_CHANGELOG=/liquibase/changelog.txt leiwang008/alpine_liquibase update
-@REM docker run --network=database_network --name alpine_liquibase --rm leiwang008/alpine_liquibase showConf
+@REM docker run --network=database_network --name alpine_liquibase --rm leiwang008/alpine_liquibase conf
 
 @REM docker run --network=database_network --rm -e LIQUIBASE_URL=jdbc:postgresql://alpine_postgres:5432/postgres  -e LIQUIBASE_USERNAME=postgres -e LIQUIBASE_PASSWORD=postgres leiwang008/alpine_liquibase update
---
+++ stop.bat
@@ -0,0 +1,13 @@
+
+@echo off
+
+echo "Stop & Remove the liquibase 'alpine_liquibase' in network 'database_network'
+docker container stop alpine_liquibase
+docker container rm alpine_liquibase
+
+echo "Stop & Remove the postgres database 'alpine_postgres' in network 'database_network'
+docker container stop alpine_postgres
+docker container rm alpine_postgres
+
+echo "Remove the network 'database_network'
+docker network rm database_network
